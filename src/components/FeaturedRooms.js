067c73fbdee8d52f8ace2d41df921a5a0749961c
Luis <luisron2007@gmail.com>
2019-12-29 16:52:57 +0100

    adding logic to context and finishing featured rooms component

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {RoomContext} from '../Context';
+import Title from './Title';
+import Loading from './Loading';
+import Room from './Room';
 
 export default class FeaturedRooms extends Component {
+    static contextType = RoomContext;
+    
     render() {
+        let {loading, featuredRooms} = this.context;
+        featuredRooms = featuredRooms.map(room => {
+            return <Room key={room.id} room={room} />
+        })
+
         return (
-            <div>
-                featured rooms
+            <section className='featured-rooms'>
+            <Title title='Featured Rooms' />
+            <div className='featured-rooms-center'>
+            {loading ? <Loading /> : featuredRooms}
             </div>
+            </section>
         )
     }
 }
