ce26375a1b67b1c9a088405159cfa02cfe19d465
Luis <luisron2007@gmail.com>
2020-01-01 20:15:22 +0100

    adding rooms list

---
+++ src/components/RoomsContainer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import RoomsFilter from './RoomsFilter';
+import RoomsList from './RoomsList';
+import { RoomConsumer } from '../Context';
+import Loading from './Loading'
+
+const RoomsContainer = () => {
+    return (
+        <RoomConsumer>
+            {
+                (value) => {
+                    const {loading,sortedRooms,rooms} = value;
+                    if(loading){
+                        return <Loading />
+                    }
+                    return (
+                        <div>
+                            rooms container
+                        <RoomsFilter rooms={rooms}/>
+                            <RoomsList rooms={sortedRooms}/>
+                        </div>
+                    )
+
+                }
+            }
+        </RoomConsumer>
+
+    );
+};
+
+export default RoomsContainer;
